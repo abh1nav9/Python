0557803516e6bab68a9a1c9831cef1a5060bb0cb
abh1nav9 <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-11-20 17:28:51 +0530

    DataTypes

---
+++ Basics/data_types.py
@@ -0,0 +1,23 @@
+# Object Types or Data Types
+
+# Numbers - 123, 1.23, 1+2j, 0b111, Decimal(), Fraction()
+
+# String - 'spam', "Bob's", b'a\x01c', u'sp\xc4m'
+
+# List - [1, 2, ['a', 'b', 'c']], list(range(10))
+
+# Tuple - (1, 'spam', 4, 'U'), tuple('spam'), namedtuple
+
+# Dictionary/Dict - {'food': 'spam', 'taste': 'yum'}, dict(hours=10)
+
+# Set - set('abc), {'a', 'b', 'c'}
+
+# File Handling -
+
+# Boolean
+
+# None - None   
+
+# Function, Modules, Classes
+
+# Advance - Decorators, Generators, Iterators, MetaProgramming 
